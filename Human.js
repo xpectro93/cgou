f654cffd26ff3763f8550fcd9923e70bca14acb1
xpectro93 <jonathanandrade@pursuit.org>
2019-10-25 00:00:09 -0400

    add initial player movement

--- Human.js
+++ Human.js
@@ -33,14 +33,53 @@ class Human{
 
 let hu = new Human('Jon', {mood:'sad', willToLive:0, strength: null, int:'minimal' })
 // console.log(hu.getMove())
-let testMap = [['  ','  ','🌱',' '],['  ','  ','🌱',' '],['  ','  ','🌱',' ']];
-testMap.forEach((row, i) => {
-    console.log(c.green.bgGreen.bold(row.join(' ')))
-})
-console.log(c.green.bgGreen(['.',',',','], c.white.bgWhite(' ,') + '!'));
+// let testMap = [['  ','  ','🌱',' '],['  ','  ','🌱',' '],['  ','  ','🌱',' ']];
+// testMap.forEach((row, i) => {
+//     console.log(c.green.bgGreen.bold(row.join(' ')))
+// })
+// console.log(c.green.bgGreen(['.',',',','], c.white.bgWhite(' ,') + '!'));
+const map = num => {
+    for(var x=[], j = 0 ; j <= num; j++){
+        x.push([])
+        for(var i = 0 ;  i <= num; i++) x[j].push(' ')
+        
+    }
+    console.log(i,j)
+        
+    return x;
+}
+let arr = map(10)
+
+// arr.forEach(row => console.log(c.green.bgGreen.bold(row.join(' '))))
+let [row, col] = [0,0]
+let move;
+while(true){
+    arr.forEach(row => console.log(c.green.bgGreen.bold(row.join(' '))))
+   move = readline.keyIn('Left or Right? ', {limit: 'wasdc'})
+   if(move==='w'){ 
+        arr[row][col]=' '
+       row--
+    arr[row][col] = '😬' }
+   else if(move ==='a'){
+       arr[row][col]=' '
+        col--
+        arr[row][col] = '😬'}
+   else if(move === 's'){
+        arr[row][col]=' '
+        row++
+        arr[row][col] = '😬'}
+   else if(move === 'd'){
+        arr[row][col]=' '
+        col++
+        arr[row][col] = '😬'}
+   console.clear()
+}
+
+
+
 // console.log(c.green.bgGreen.bold())
 // Human.randomCreate(20)
-console.log(Human.randomCreate(5))
+// console.log(Human.randomCreate(5))
 module.exports = Human;
 // '🧟'
 // '🧝'
