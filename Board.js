863d2c3949fd249bce2f4fff3436033133ea8d58
xpectro93 <jonathanandrade@pursuit.org>
2019-10-27 16:05:11 -0400

    add board test

---
+++ Board.js
@@ -0,0 +1,12 @@
+
+class Board {
+    constructor(){
+        this.grid = [];
+        this.points = 0;
+    }
+    createBoard(){
+        
+    }
+}
+
+module.exports = Board;
--- Human.js
+++ Human.js
@@ -4,6 +4,13 @@ const Syms = require('./Syms.js');
 const c = require('chalk');
 
 const random = item => item[Math.floor(Math.random()* item.length)]
+// '🔷'
+// '🔸'
+// '⚪' 
+// '🍒'
+
+
+for( )
 
 
 class Human{
@@ -39,55 +46,104 @@ let hu = new Human('Jon', {mood:'sad', willToLive:0, strength: null, int:'minima
 // })
 // console.log(c.green.bgGreen(['.',',',','], c.white.bgWhite(' ,') + '!'));
 const map = num => {
-    for(var x=[], j = 0 ; j <= num; j++){
-        x.push([])
-        for(var i = 0 ;  i <= num; i++) x[j].push(' ')
+    let grid =[]
+    for(let j = 0 ; j <= num; j++){
+        grid.push([])
+        for(let i = 0 ;  i <= num; i++){ 
+            if(j === 0){
+            grid[j].push('🔷')
+            }
+           
+            
+            grid[j].push(' ')
+        }
         
     }
-    console.log(i,j)
+    
         
-    return x;
+    return grid;
 }
-let arr = map(10)
+let arr = map(20)
 
-// arr.forEach(row => console.log(c.green.bgGreen.bold(row.join(' '))))
-let [row, col] = [0,0]
-let move;
+
+arr.forEach(row => console.log(c.green.bgGreen.bold(row.join(' '))))
+let [row, col] = [1,1]
+let move = 'd'
 // c.green.bgGreen.bold(row.join(' '))
 
-while(true){
-    arr.forEach(row => console.log(row.join('🌱')))
-    console.log(row,col)
-   move = readline.keyIn('Left or Right? ', {limit: 'wasdc'})
-    
-   if(move==='w'){ 
-     if(row - 1 >= 0){
-            arr[row][col]=' '
-            row--
-            arr[row][col] = '😬' 
-     } 
-    }else if(move ==='a'){
-       if(col - 1 >= 0){
-            arr[row][col]=' '
-            col--
-            arr[row][col] = '😬'
-       }    
-    }else if(move === 's'){
-        if(row + 1 < arr.length){
-            arr[row][col]=' '
-            row++
-            arr[row][col] = '😬'
-        }
-    }else if(move === 'd'){
-        if(col + 1 < arr.length){
-            arr[row][col]=' '
-            col++
-            arr[row][col] = '😬'
-        }
-    }
+    // move = readline.keyIn('[w]up, [a]left, [s]down, [d]right', {limit: 'wasd'})
+    // setInterval(()=>{ 
+    //     // console.clear();
+    //     // arr.forEach(row => console.log(row.join('')))
+
+ 
+    //    if(move==='w'){ 
+    //      if(row - 1 >= 0){
+    //             arr[row][col]=' '
+    //             row--
+    //             arr[row][col] = '😬' 
+    //      } 
+    //     }else if(move ==='a'){
+    //        if(col - 1 >= 0){
+    //             arr[row][col]=' '
+    //             col--
+    //             arr[row][col] = '😬'
+    //        }    
+    //     }else if(move === 's'){
+    //         if(row + 1 < arr.length){
+    //             arr[row][col]=' '
+    //             row++
+    //             arr[row][col] = '😬'
+    //         }
+    //     }else if(move === 'd'){
+    //         if(col + 1 < arr.length){
+    //             arr[row][col]=' '
+    //             col++
+    //             arr[row][col] = '😬'
+    //         }
+    //     }
         
-   console.clear()
-}
+    
+    // }, 750);
+
+
+// let count = 0
+
+// while(true){
+    
+
+//     arr.forEach(row => console.log(row.join('🌱')))
+//     console.log(row,col)
+//     move = readline.keyIn('[w]up, [a]left, [s]down, [d]right', {limit: 'wasdc'})
+    
+//    if(move==='w'){ 
+//      if(row - 1 >= 0){
+//             arr[row][col]=' '
+//             row--
+//             arr[row][col] = '😬' 
+//      } 
+//     }else if(move ==='a'){
+//        if(col - 1 >= 0){
+//             arr[row][col]=' '
+//             col--
+//             arr[row][col] = '😬'
+//        }    
+//     }else if(move === 's'){
+//         if(row + 1 < arr.length){
+//             arr[row][col]=' '
+//             row++
+//             arr[row][col] = '😬'
+//         }
+//     }else if(move === 'd'){
+//         if(col + 1 < arr.length){
+//             arr[row][col]=' '
+//             col++
+//             arr[row][col] = '😬'
+//         }
+//     }
+        
+//    console.clear()
+// }
 
 
 
